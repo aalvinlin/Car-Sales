04896cd4a425e6ea5d033e1f1e62987275ae0598
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-01-22 13:38:49 -0800

    Provider wrapped, store created, statsReducer created

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+
+import baseStatsReducer from "./reducers/baseStatsReducer";
+import extrasReducer from "./reducers/extrasReducer";
+
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const rootReducer = combineReducers({
+    stats: baseStatsReducer,
+    extras: extrasReducer
+})
+const store = createStore(rootReducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , rootElement);
---
+++ src/reducers/statsReducer.js
@@ -0,0 +1,108 @@
+// export function addCarId(id) {
+//     return {
+//         type: "ADD_CAR_ID",
+//         payload: id
+//     }
+// }
+
+// export function addCarYear(year) {
+//     return {
+//         type: "ADD_CAR_YEAR",
+//         payload: year
+//     }
+// }
+
+// export function addCarMake(make) {
+//     return {
+//         type: "ADD_CAR_MAKE",
+//         payload: make
+//     }
+// }
+
+// export function addCarModel(model) {
+//     return {
+//         type: "ADD_CAR_MODEL",
+//         payload: model
+//     }
+// }
+
+// export function addPictureUrl(url) {
+//     return {
+//         type: "ADD_PICTURE_URL",
+//         payload: url
+//     }
+// }
+
+// export function addBasePrice(price) {
+//     return {
+//         type: "ADD_BASE_PRICE",
+//         payload: price
+//     }
+// }
+
+export function addFeature(featureId) {
+    return {
+        type: "ADD_FEATURE",
+        payload: featureId
+    }
+}
+
+export function removeFeature(featureId) {
+    return {
+        type: "REMOVE_FEATURE",
+        payload: featureId
+    }
+}
+
+const initialStats = {
+    additionalPrice: 0,
+    car: {
+      price: 26395,
+      name: '2019 Ford Mustang',
+      image:
+        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+      features: []
+    },
+    additionalFeatures: [
+      { id: 1, name: 'V-6 engine', price: 1500 },
+      { id: 2, name: 'Racing detail package', price: 1500 },
+      { id: 3, name: 'Premium sound system', price: 500 },
+      { id: 4, name: 'Rear spoiler', price: 250 }
+    ]
+  };
+
+export default function statsReducer(stats = initialStats, action) {
+
+    // Removes an object with a specified targetId from array1 and adds it to array2.
+    function moveFromArray(targetId, array1, array2) {
+
+        let object = array1.filter(item => item.id === targetId)[0];
+
+        // if object not found, return both arrays as is
+        if (object.length === 0)
+            { return [array1, array2]}
+
+        let array1WithItemRemoved = array1.filter(item => item.id !== targetId);
+        let array2WithItemAdded = [...array2, object];
+
+        return [array1WithItemRemoved, array2WithItemAdded];
+    }
+
+    switch (action.type)
+    {
+        case "ADD_FEATURE":
+
+            const [newFeaturesOnCar, newAvailableFeatures] = moveFromArray(action.payload, stats.car.features, stats.car.additionalFeatures);
+
+            return { ...stats, car: {...car, newFeaturesOnCar}, additionalFeatures: newAvailableFeatures}
+ 
+        case "REMOVE_FEATURE":
+
+            const [newFeaturesOnCar, newAvailableFeatures] = moveFromArray(action.payload, stats.car.additionalFeatures, stats.car.features);
+
+            return { ...stats, car: {...car, newFeaturesOnCar}, additionalFeatures: newAvailableFeatures}
+
+        default:
+            return stats;
+    }
+}
