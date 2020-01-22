7f6605f0977f5f87cbb0f70812cd8ca13f411f4d
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-01-22 13:40:27 -0800

    changed rootReducer to only use statsReducer for now

--- src/index.js
+++ src/index.js
@@ -4,18 +4,21 @@ import ReactDOM from 'react-dom';
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 
-import baseStatsReducer from "./reducers/baseStatsReducer";
-import extrasReducer from "./reducers/extrasReducer";
+import statsReducer from "./reducers/statsReducer";
+// import extrasReducer from "./reducers/extrasReducer";
 
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-const rootReducer = combineReducers({
-    stats: baseStatsReducer,
-    extras: extrasReducer
-})
+// const rootReducer = combineReducers({
+//     stats: baseStatsReducer,
+//     extras: extrasReducer
+// })
+
+const rootReducer = statsReducer;
+
 const store = createStore(rootReducer);
 
 const rootElement = document.getElementById('root');
